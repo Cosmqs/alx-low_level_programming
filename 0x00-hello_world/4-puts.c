41b474849e50039903c8a837a02b4c6a6d5dd5bc
Mwenda <Mwendacosmas4@gmail.com>
2023-02-16 08:15:23 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-int puts(const char* "Programming is like building a multilingual puzzle");
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
