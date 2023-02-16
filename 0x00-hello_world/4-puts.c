6d2e674ee60de78796e2f64a1353ee32d993c38a
Mwenda <Mwendacosmas4@gmail.com>
2023-02-16 08:35:11 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like buiding a multilingual puzzle, followed by a new line.
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
