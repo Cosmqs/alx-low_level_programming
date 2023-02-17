7fc60b58512e21e2239bf5c73caa23ed76211c5b
Mwenda <Mwendacosmas4@gmail.com>
2023-02-17 11:35:41 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && 'q')
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
