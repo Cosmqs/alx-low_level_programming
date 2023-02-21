8dc3eec46c79bee30fd7752ede5842bb28739f63
Mwenda <Mwendacosmas4@gmail.com>
2023-02-21 13:26:18 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints last digit of a number
+ * @i: Parameter to be measured
+ *
+ * Return: Always y (Success)
+ */
+
+int print_last_digit(int i)
+{
+	int y;
+
+	y = i % 10;
+	if (i >= 0)
+	{
+		y = y;
+		_putchar(y + '0');
+	}
+	return (y);
+}
+
