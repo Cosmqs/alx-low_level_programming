a2c16a07809be15046e605c012d50339397895d6
Mwenda <Mwendacosmas4@gmail.com>
2023-02-23 07:51:28 -0800

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,17 +3,17 @@
 
 /**
  * print_numbers - prints from 0 - 9
- *@a: parameter to be measured
- * 
+ *
  * Return: 0.
  */
 
 void print_numbers(void)
 {
 	int x;
+
 	for (x = 0; x <= 9; x++)
 	{
 		_putchar(x + '0');
 	}
 	_putchar('\n');
-}	
+}
