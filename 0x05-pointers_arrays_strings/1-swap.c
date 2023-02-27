61fa1f6e65feaa8fadcab6d6e54f455b5f765515
Mwenda <Mwendacosmas4@gmail.com>
2023-02-27 08:10:36 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps values
+ * @a: parameter 1
+ * @b: parameter 2
+ *
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int q;
+
+	*a  = q;
+	*a = *b;
+	*b = q;
+
+}
