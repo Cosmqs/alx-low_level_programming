a5e45c627728cce0d4d81ef63887f9c0198560fe
Mwenda <Mwendacosmas4@gmail.com>
2023-03-08 03:07:05 -0800

    reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,20 +2,18 @@
 
 /**
  * _print_rev_recursion - prints string in reverse
- * @s: string to be reversed
+ * @s: string
  *
  * Return: nothing
  */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return;
+		s++;
+		_print_rev_recursion(s);
+		s--;
+		_putchar(*s);
 	}
-	s++;
-	_print_rev_recursion(s);
-	s--;
-	_putchar(*s);
 }
