4b7746ba707e202c0f3fe52fa4621fc3f26c6f8b
Mwenda <Mwendacosmas4@gmail.com>
2023-03-13 07:26:24 -0700

    new

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,7 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
 	{
 		s2 = "";
@@ -31,14 +29,11 @@ char *str_concat(char *s1, char *s2)
 	}
 	l = i + j;
 	s = malloc((sizeof(char) * l) + 1);
-	
 	if (s == NULL)
 	{
 		return (NULL);
 	}
-
 	j = 0;
-
 	while (k < l)
 	{
 		if (k <= i)
