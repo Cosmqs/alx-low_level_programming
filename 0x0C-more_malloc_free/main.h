400851ac9986d8410e4750824a44235747ab54b1
Mwenda <Mwendacosmas4@gmail.com>
2023-03-16 04:50:55 -0700

    _calloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <limits.h>
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 #endif /* MAIN_H */
