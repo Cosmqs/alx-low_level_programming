887f4fbe2eea860025ec6bc6c45ea0e7303b48fd
Mwenda <Mwendacosmas4@gmail.com>
2023-03-16 05:04:25 -0700

    array

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,6 +5,7 @@
 #include <limits.h>
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 #endif /* MAIN_H */
