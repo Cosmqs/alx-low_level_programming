aeac9aefceaf3f274977ca6e0da54d6d90913595
Mwenda <Mwendacosmas4@gmail.com>
2023-03-20 05:14:14 -0700

    FILE

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	exit (EXIT_SUCCESS);
+}
