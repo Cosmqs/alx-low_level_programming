683612d12178550a11df3fa22799fd6af4cdb43c
Mwenda <Mwendacosmas4@gmail.com>
2023-03-20 10:26:55 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - poppy
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description - longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
