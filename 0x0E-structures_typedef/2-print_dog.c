354af1aba15ef6b33b602efb493c7737679d633b
Mwenda <Mwendacosmas4@gmail.com>
2023-03-20 11:32:14 -0700

    poppy

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,6 +1,6 @@
-#include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "dog.h"
 
 /**
  * print_dog - poppy
@@ -24,7 +24,7 @@ void print_dog(struct dog *d)
 	{
 		d->owner = "Owner: (nil)";
 	}
-	printf("Name:  %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
 
 
