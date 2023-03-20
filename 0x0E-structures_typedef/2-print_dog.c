848d40e2cea08e01dba01e8b8f0173af24546718
Mwenda <Mwendacosmas4@gmail.com>
2023-03-20 11:36:52 -0700

    poppy

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,13 +18,11 @@ void print_dog(struct dog *d)
 	}
 	if (d->name == NULL)
 	{
-		d->name = "Name: (nil)";
+		d->name = "(nil)";
 	}
 	if (d->owner == NULL)
 	{
-		d->owner = "Owner: (nil)";
+		d->owner = "(nil)";
 	}
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
-
-
