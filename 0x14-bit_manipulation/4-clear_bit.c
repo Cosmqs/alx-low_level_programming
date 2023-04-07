ba7454604317f09bc222ff8046ddb33639e75289
Mwenda <Mwendacosmas4@gmail.com>
2023-04-07 06:08:58 -0700

    clear bits

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,12 +11,12 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int temp;
-	
+
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
-	
+
 	temp = ~(1UL << index);
 
 	*n &= temp;
